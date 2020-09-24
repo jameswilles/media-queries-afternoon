616a6a45cc1aa6b8fd86121274a4a3ca76e7a42b
jameswilles <james.m.willes@gmail.com>
2020-09-23 19:27:06 -0600

    final push

--- greyscale-copy/src/App.js
+++ greyscale-copy/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      menu: false,
+    }
+  }
+
+  toggleMenu = () => {
+    this.setState({ menu: !this.state.menu })
+  }
+
+  render(){
+    return (
+     <div className='app'>
+        <header className='nav-bar'>
+         <h1 className='nav-bar-title'> Start Bootstrap </h1>
+         <div className='nav-bar-menu'>
+            <a href='#'> About </a>
+            <a href='#'> Projects </a>
+            <a href='#'> Content </a>
+          </div>
+         <div className='nav-bar-icon' onClick={this.toggleMenu}> Menu &#9776; </div>
+       </header>
+       <div className={this.state.menu ? 'menu slide' : 'do-not-display'}>
+          <a href='#'> About </a>
+          <a href='#'> Projects </a>
+          <a href='#'> Content </a>
+        </div>
+        <div className='main'>
+         <h1 className='big-title'> GREYSCALE </h1>
+         <h2 className='smol-title'> A free, responsive, one page Bootstrap theme created by Start Bootstrap </h2>
+         <div className='start-button'> GET STARTED </div>
+       </div>
+     </div>
+    );
+  }
 }
 
 export default App;
